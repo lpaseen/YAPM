1607a0323fe279414fdb0c9660dd548200482c19
lpaseen <peters-github@techwiz.ca>
2013-06-05 00:59:34 -0400

    use subdir under DONE

--- rpi/Load_data_to_mysql.sh
+++ rpi/Load_data_to_mysql.sh
@@ -3,11 +3,20 @@
 #
 #
 cd ~/powermon/logs
-if ls powermon_data_2013-*csv &>/dev/null;then
+if ls powermon_data_201?-*csv &>/dev/null;then
+    echo
+    echo "================================================================"
     date +%F\ %T
-    mv powermon_data_2013-*csv TODO
+    mv powermon_data_201?-*csv TODO
     cd TODO
-    cat powermon_data_*.csv|~/powermon/Load_data.pl 
-    mv powermon_data_2013-*csv ../DONE/
+    cat powermon_data_*.csv|~/powermon/Load_data.pl
+    # Update the summary table with latest news
+    mysql -upowermon -penergymon powermon -e "source ~/powermon/Fillin_summarytable.sql"
+    # FIXME, the files moved might been created last month
+    # mv powermon_data_201?-*csv ../DONE/$(date +%Y-%m)
+    for i in powermon_data_201?-*csv;do
+	DSTYM=$(echo "$i" |sed 's/.*data_20\([0-9]\)/20\1/'|cut -d- -f1,2)
+	mv $i ../DONE/$DSTYM/
+    done
     cd ~/powermon
 fi
